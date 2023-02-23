86594326505cd6bf1aecbbde3eb8280bfa93885b
Zhengqbbb <1074059947@qq.com>
2023-02-23 10:49:47 +0800

    feat: initial commit

--- decrypt.js
+++ decrypt.js
@@ -7,14 +7,13 @@ const encryptedMessageBase64 = 'ZLN5Cu09JSUW1X7dG4qnvpGWlpnW9RMJedMNmT0D0DjqxL6r
 const publicKeyBase64 = 'b7KAEVtR5MvuV5dJilUJigeZOvSI0VNUefFPDjVvLhs='
 const privateKeyBase64 = 'I25HCqHxo/ND9c2O0mIJIZgGq+SqbyVnYHqKwDgZQSc='
 
-// 解码 base64 字符串为二进制数据
-const publicKey = Uint8Array.from(atob(publicKeyBase64), c => c.charCodeAt(0));
-const privateKey = Uint8Array.from(atob(privateKeyBase64), c => c.charCodeAt(0));
-const encryptedMessage = Uint8Array.from(atob(encryptedMessageBase64), c => c.charCodeAt(0));
+sodium.ready.then(() => {
+  // 解码 base64 字符串为二进制数据
+  // same => const publicKey = Uint8Array.from(atob(publicKeyBase64), c => c.charCodeAt(0))
+  const publicKey = sodium.from_base64(publicKeyBase64, sodium.base64_variants.ORIGINAL)
+  const privateKey = sodium.from_base64(privateKeyBase64, sodium.base64_variants.ORIGINAL)
+  const encryptedMessage = sodium.from_base64(encryptedMessageBase64, sodium.base64_variants.ORIGINAL)
 
-(async () => {
-  await sodium.ready;
-  // 使用 crypto_box_seal_open 函数对加密消息进行解密
-  const decryptedMessage = sodium.crypto_box_seal_open(encryptedMessage, publicKey, privateKey, 'text');
-  console.log(decryptedMessage);
-})();
+  const decryptedMessage = sodium.crypto_box_seal_open(encryptedMessage, publicKey, privateKey);
+  console.log(sodium.to_string(decryptedMessage));
+});
